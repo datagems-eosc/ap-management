38592edfdc2173bfe500b1f48bfe22b7f5e41d5e
SoTrx <11771975+SoTrx@users.noreply.github.com>
2026-03-16 11:52:20 +0100

    chore: Remove tags from API router initialization in routes.py

--- ap_management/api/v1/routes.py
+++ ap_management/api/v1/routes.py
@@ -5,8 +5,7 @@ from .health import health_check
 from .tasks.routes import router as tasks_routes
 
 router = APIRouter(
-    prefix="/api/v1",
-    tags=["v1"],
+    prefix="/api/v1"
 )
 router.include_router(aps_routes)
 router.include_router(tasks_routes)
